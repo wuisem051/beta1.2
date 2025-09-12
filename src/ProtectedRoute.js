92626aaac19a10582ec860e6255c9124de2d7cfe
wuisem abou <43572427+wuisem051@users.noreply.github.com>
2025-09-12 15:54:08 -0400

    fix: Corregir redirección después del inicio de sesión en ProtectedRoute

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -3,10 +3,15 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
-  const { currentUser } = useAuth();
+  const { currentUser, loading } = useAuth(); // Obtener el estado de carga
 
   // Permitir acceso si REACT_APP_DEV_MODE está activado (solo para desarrollo/pruebas)
   const isDevMode = process.env.REACT_APP_DEV_MODE === 'true';
+
+  if (loading) {
+    // Mostrar un indicador de carga mientras se verifica el estado de autenticación
+    return <div>Cargando autenticación...</div>;
+  }
 
   if (!currentUser && !isDevMode) {
     // Si no hay usuario y no estamos en modo de desarrollo, redirigir a la página de login
@@ -14,7 +19,6 @@ const ProtectedRoute = ({ children }) => {
   }
 
   // Si hay usuario o estamos en modo de desarrollo, permitir el acceso
-
   return children;
 };
 
